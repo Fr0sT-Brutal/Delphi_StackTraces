46e5fd06b5ac86eeb2dcb159123dbe5e5649dfbf
Fr0sT <fr0st@local>
2022-12-14 12:16:28 +0300

    Demo project: + add comments and messages, add sample of random callstack

--- StackTraceSample.dpr
+++ StackTraceSample.dpr
@@ -25,25 +25,49 @@ begin
   Nested1;
 end;
 
+procedure Nested1_1;
+var CallStack: TDbgInfoStack;
+begin
+  GetCallStackOS(CallStack, 0);
+  Writeln('** Random callstack (no exception involved):', NL, CallStackToStr(CallStack));
+end;
+
+procedure Nested0_1;
+begin
+  Nested1_1;
+end;
+
 var
   sl: TStringList;
   ai: TMapFileAddrInfo;
 begin
+  // Setup our custom exception call stack mechanism
   InstallExceptionCallStack;
+
+  // Load MAP file
   sl := TStringList.Create;
   sl.LoadFromFile(ChangeFileExt(ParamStr(0), '.map'), TEncoding.UTF8);
   ReadMapFile(sl.Text);
   sl.Free;
+
+  // Demo1: get info about some address
   GetAddrInfo(@Nested2, ai);
-  Writeln(AddrInfoToString(ai));
-  Readln;
+  Writeln('** Address info of Nested2 proc:', NL, AddrInfoToString(ai));
+
+  // Demo2: exception stack trace info
   try
     Nested0;
   except on E: Exception do
     begin
-      Writeln(ExceptionInfo(E, True));
-      Readln;
+      Writeln('** Exception info & stack trace:', NL, ExceptionInfo(E, True));
     end;
   end;
+
+  // Demo3: random callstack (no exception involved)
+  Nested0_1;
+
+  // Remove our custom exception call stack mechanism
   UninstallExceptionCallStack;
+
+  Readln;
 end.
